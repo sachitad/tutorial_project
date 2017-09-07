e786b255213ded756c2d65e6266292b15cb0d674
Sachit Adhikari <sachitad@Sachits-MBP.gateway>
2017-09-07 22:28:59 +1000

    Homepage with materializecss basic template

--- tutorial/views.py
+++ tutorial/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    """
+    This is the homepage, I would like to call main page of my website Index
+    """
+    template_name = 'tutorial/index.html'
